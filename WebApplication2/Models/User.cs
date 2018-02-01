24cb5e504a5542faf6e0c8bdcf73fdf81eeba59a
Kopp <c-davkopp@PA.gov>
2018-02-01 17:12:23 -0500

    initial commit

---
+++ WebApplication2/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class User
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        public bool IsValid(string _username, string _password) => true;
+    }
+}
